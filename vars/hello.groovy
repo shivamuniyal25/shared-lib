be48ce3bdb96cc0a5e9747374fa5a1791c9e78be
Shivam <shivamuniyal20@gmail.com>
2026-01-07 20:52:30 +0530

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -2,7 +2,7 @@ def call() {
     echo "Step 1: Function called from vars directory"
 
     def helper = new helpers.message()
-    helper.showMessage()
+    echo helper.showMessage()
 
     def configText = libraryResource 'config.txt'
     echo "Step 3: Config file content:"
